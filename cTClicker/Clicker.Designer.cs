4c0b87c959393520f7b00146bf8fd1a97d10b23c
CoffeeWhite7 <77559294+CoffeeWhite7@users.noreply.github.com>
2022-12-21 14:34:00 +0800

    Add files via upload

---
+++ cTClicker/Clicker.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace cTClicker
+{
+    partial class Clicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Clicker));
+            this.label1 = new System.Windows.Forms.Label();
+            this.clicktimeUpDown = new System.Windows.Forms.NumericUpDown();
+            this.clickMode = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.click_tmr = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.clicktimeUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑 Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(14, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "点击间隔(ms)";
+            // 
+            // clicktimeUpDown
+            // 
+            this.clicktimeUpDown.Location = new System.Drawing.Point(144, 13);
+            this.clicktimeUpDown.Margin = new System.Windows.Forms.Padding(4);
+            this.clicktimeUpDown.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.clicktimeUpDown.Name = "clicktimeUpDown";
+            this.clicktimeUpDown.Size = new System.Drawing.Size(140, 23);
+            this.clicktimeUpDown.TabIndex = 1;
+            this.clicktimeUpDown.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // clickMode
+            // 
+            this.clickMode.FormattingEnabled = true;
+            this.clickMode.Items.AddRange(new object[] {
+            "左键",
+            "右键",
+            "左键+右键",
+            "中键"});
+            this.clickMode.Location = new System.Drawing.Point(144, 52);
+            this.clickMode.Margin = new System.Windows.Forms.Padding(4);
+            this.clickMode.Name = "clickMode";
+            this.clickMode.Size = new System.Drawing.Size(140, 25);
+            this.clickMode.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑 Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(15, 51);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "点击模式";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑 Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(15, 87);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(271, 40);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Alt+C开始/停止点击，Ctrl+Alt+E关闭窗口\r\n              Shift+C开启防挡模式";
+            // 
+            // click_tmr
+            // 
+            this.click_tmr.Tick += new System.EventHandler(this.click_tmr_Tick);
+            // 
+            // Clicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(299, 136);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.clickMode);
+            this.Controls.Add(this.clicktimeUpDown);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Clicker";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Clicker";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.WhiteSmoke;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Clicker_FormClosing);
+            this.Load += new System.EventHandler(this.Clicker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.clicktimeUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown clicktimeUpDown;
+        private System.Windows.Forms.ComboBox clickMode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer click_tmr;
+    }
+}
---
+++ cTClicker/Clicker.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualBasic.Devices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace cTClicker
+{
+    public partial class Clicker : Form
+    {
+        public Clicker()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+            clickMode.SelectedIndex = 0;
+            clicktimeUpDown.Value = 30;
+            
+        }
+
+        HotKeys hotKsys = new HotKeys();
+
+        private void Clicker_Load(object sender, EventArgs e)
+        {
+            hotKsys.Regist(this.Handle, (int)HotKeys.HotkeyModifiers.Alt, Keys.C, StartClick);//Alt+C
+            hotKsys.Regist(this.Handle, (int)HotKeys.HotkeyModifiers.Control + (int)HotKeys.HotkeyModifiers.Alt, Keys.E, Exit);//Ctrl+Alt+E
+            hotKsys.Regist(this.Handle, (int)HotKeys.HotkeyModifiers.Shift, Keys.C, ColorT);//Shift+C
+        }
+
+
+
+
+        void StartClick()
+        {
+            
+            if(clicktimeUpDown.Value <= 0) { clicktimeUpDown.Value = 1; }
+            click_tmr.Interval = (int)clicktimeUpDown.Value;
+            if (click_tmr.Enabled)
+            {
+                click_tmr.Enabled = false;
+                this.Enabled = true;
+            }
+            else
+            {
+                click_tmr.Enabled = true;
+                this.Enabled = false;
+            }
+        }
+
+        void Exit()
+        {
+            Application.Exit();
+        }
+
+        void ColorT()
+        {
+            if(this.BackColor != Color.WhiteSmoke)
+            { 
+                this.BackColor = Color.WhiteSmoke;
+            }
+            else
+            {
+                this.BackColor = Color.FromArgb(255,240,240,240);
+            }
+            
+
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            hotKsys.ProcessHotKey(m);
+            base.WndProc(ref m);
+        }
+
+        private void Clicker_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            hotKsys.UnRegist(this.Handle, StartClick);
+        }
+        class Mouse
+        {
+            [System.Runtime.InteropServices.DllImport("user32")]
+            public static extern int mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+            //移动鼠标 
+            public const int MOUSEEVENTF_MOVE = 0x0001;
+            //模拟鼠标左键按下 
+            public const int MOUSEEVENTF_LEFTDOWN = 0x0002; 
+            //模拟鼠标左键抬起 
+            public const int MOUSEEVENTF_LEFTUP = 0x0004;
+            //模拟鼠标右键按下 
+            public const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+            //模拟鼠标右键抬起 
+            public const int MOUSEEVENTF_RIGHTUP = 0x0010;
+            //模拟鼠标中键按下 
+            public const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+            //模拟鼠标中键抬起 
+            public const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+            //标示是否采用绝对坐标 
+            public const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+            [DllImport("user32.dll")]
+            public static extern bool SetCursorPos(int X, int Y);
+        }
+
+        private void click_tmr_Tick(object sender, EventArgs e)
+        {
+            if     (clickMode.SelectedIndex== 0) {Mouse.mouse_event(Mouse.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                                                  Mouse.mouse_event(Mouse.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);}//左键
+            else if(clickMode.SelectedIndex== 1) {Mouse.mouse_event(Mouse.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+                                                  Mouse.mouse_event(Mouse.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);}//右
+            else if(clickMode.SelectedIndex== 2) {Mouse.mouse_event(Mouse.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                                                  Mouse.mouse_event(Mouse.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                                                  Mouse.mouse_event(Mouse.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+                                                  Mouse.mouse_event(Mouse.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);}//左+右
+            else if(clickMode.SelectedIndex== 3) {Mouse.mouse_event(Mouse.MOUSEEVENTF_MIDDLEDOWN, 0, 0, 0, 0);
+                                                  Mouse.mouse_event(Mouse.MOUSEEVENTF_MIDDLEUP, 0, 0, 0, 0);}//中
+            
+        }
+    }
+}
